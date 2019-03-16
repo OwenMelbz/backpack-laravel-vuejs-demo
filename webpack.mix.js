8928899cef073240d5d27b593ad1ac46482ac7c8
Owen Melbourne <owenmelbz@gmail.com>
2019-03-16 12:35:30 +0000

    Step 1: Add a new asset pipeline for your backpack assets using Laravel Mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,3 +13,6 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
    .sass('resources/sass/app.scss', 'public/css');
+
+mix.js('resources/js/backpack.js', 'public/js')
+    .sass('resources/sass/backpack.scss', 'public/css');
