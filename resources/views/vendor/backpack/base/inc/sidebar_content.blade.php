426276dd89e485aedc645c86a2f0364a06acb782
Owen Melbourne <owenmelbz@gmail.com>
2019-03-16 11:27:01 +0000

    Clean Backpack install with User crud

---
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -0,0 +1,4 @@
+<!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
+<li><a href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard"></i> <span>{{ trans('backpack::base.dashboard') }}</span></a></li>
+<li><a href="{{ backpack_url('elfinder') }}"><i class="fa fa-files-o"></i> <span>{{ trans('backpack::crud.file_manager') }}</span></a></li>
+<li><a href='{{ backpack_url('user') }}'><i class='fa fa-user'></i> <span>Users</span></a></li>
