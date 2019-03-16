1f7aaba0d46b6fae18fcca737f1c245457a1bd91
Owen Melbourne <owenmelbz@gmail.com>
2019-03-16 12:50:09 +0000

    Step 6: We then wrap the input field with a custom component which we'll create later.
    We pass in the original input field as a slot so we can update the value of the input via our Vue component

--- resources/views/vendor/backpack/crud/fields/password_meter.blade.php
+++ resources/views/vendor/backpack/crud/fields/password_meter.blade.php
@@ -2,8 +2,9 @@
 <div @include('crud::inc.field_wrapper_attributes') >
     <label>{!! $field['label'] !!}</label>
     @include('crud::inc.field_translatable_icon')
-
-    <input type="password" name="{{ $field['name'] }}">
+    <password-meter _value="{{ old(square_brackets_to_dots($field['name'])) ?? $field['value'] ?? $field['default'] ?? '' }}">
+        <input type="password" name="{{ $field['name'] }}">
+    </password-meter>
 
     {{-- HINT --}}
     @if (isset($field['hint']))
