ee26c403816b53405aa5c821e6e1825ebcd414d2
Owen Melbourne <owenmelbz@gmail.com>
2019-03-16 12:48:54 +0000

    Step 5: We replicate an existing input field to make sure we still have the ability to use things like "hints"

---
+++ resources/views/vendor/backpack/crud/fields/password_meter.blade.php
@@ -0,0 +1,12 @@
+<!-- password -->
+<div @include('crud::inc.field_wrapper_attributes') >
+    <label>{!! $field['label'] !!}</label>
+    @include('crud::inc.field_translatable_icon')
+
+    <input type="password" name="{{ $field['name'] }}">
+
+    {{-- HINT --}}
+    @if (isset($field['hint']))
+        <p class="help-block">{!! $field['hint'] !!}</p>
+    @endif
+</div>
